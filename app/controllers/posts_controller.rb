f161e6a271354f257d307f3eb3bbc35e4197c8b8
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-05 17:01:59 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,20 +12,22 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
-	  redirect_to post_path(@post)
+		@post = Post.create(post_params)
+		redirect_to post_path(@post)
 	end
-
+	
 	def edit
 		@post = Post.find(params[:id])
 	end
-
+	
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(params.require(:post).permit(:title, :description))
 	  redirect_to post_path(@post)
+	end
+
+	private
+	def post_params
+		params.permit(:title, :description)
 	end
 end
